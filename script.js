f829b4fbad431c92f2e62370dcea764afed64076
Jucian Kauê Decezare <jucian_decezare2015@hotmail.com>
2023-06-09 16:57:11 -0300

    v.01

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ import { GLTFLoader } from './libs/three/jsm/GLTFLoader.js';
 import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
 import { ARButton } from './libs/three/jsm/ARButton.js'
 
-const renderer = new THREE.WebGLRenderer();
-
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
 
 document.body.appendChild(renderer.domElement);
 
@@ -18,33 +18,48 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
-
 camera.position.set(0, 2, 5);
+
+// const axesHelper = new THREE.AxesHelper(3);
+// scene.add(axesHelper);
 
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
 scene.add(ambientLight);
 
-const light = new THREE.DirectionalLight( 0xffffff );
-light.position.set( 1, 1, 1 ).normalize();
-scene.add( light );
+// const light = new THREE.DirectionalLight( 0xffffff );
+// light.position.set( 1, 1, 1 ).normalize();
+// scene.add( light );
 
+function onSelect() {
+    console.log('selected');
+
+    const loader = new GLTFLoader();
+
+    loader.load(
+        'cat.gltf',
+        function (gltf) {
+            const object = gltf.scene;
+            object.scale.set(4, 4, 4);
+            object.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+            scene.add(object);
+            renderer.render(scene, camera);
+            
+        }, undefined, undefined
+    );
+}
+
+let controller = renderer.xr.getController(0);
+controller.addEventListener('select', onSelect)
+scene.add( controller );
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(0, 3, 0);
+scene.add(box);
 
 renderer.xr.enabled = true;
 
 document.body.appendChild(ARButton.createButton(renderer));
-
-const loader = new GLTFLoader();
-
-loader.load(
-    'cat.glb',
-    function (gltf) {
-
-        scene.add(gltf.scene);
-
-    }
-)
 
 renderer.render(scene, camera);
